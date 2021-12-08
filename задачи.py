fda9f83ddc42dba85532f76052123727b2eaa59b
Lidia <Lidia@DESKTOP-QCVC1QJ>
2021-12-08 19:32:57 +0200

    Add project files.

---
+++ задачи.py
@@ -0,0 +1,48 @@
+#print("sisestage number")
+#k = [ " (\_/)  ",
+      #" (o o)  ",
+      #" / | \* " ]
+#n=int(input())
+#for i in k:
+    #print(i*n)
+
+#n=int(input("sisestage viimane number= "))
+#s=0
+#for i in range(0,n+1):
+    #s=i+s 
+#print("summa 0 kuni", n, "=", s)
+
+#import random
+#x=0
+#a=random.randint(0,100)
+#while x<10:
+    #b=int(input("number: "))
+    #if b<a:
+        #print("vaiksem!")
+    #elif b>a:
+        #print("suurem!")
+    #elif b==a:
+        #print("oige!")
+        #break
+    #x+=1
+    #print("see number:"+str(a))
+
+#n1=int(input("sisestage tais number: "))
+#n2=0
+#while n1>0:
+    #digit = n1 % 10
+    #n1=n1//10
+    #n2=n2*10
+    #n2=n2 + digit
+#print("vastupidine number: ", n2)
+
+#n=int(input("sisestage tais number: "))
+#sum=0
+#mult=1
+#while n >0:
+    #digit=n%10
+    #sum = sum+digit 
+    #mult=mult*digit
+    #n=n//10
+#print("summa on: ", sum)
+#print("korrutis on: ", mult)
